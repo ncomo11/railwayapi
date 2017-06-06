9197006ae589d01acb3026432e26b2b51d99ae00
praveen10 <praveen_jhansi@yahoo.co.in>
2017-06-06 14:15:51 +0530

    Update app.js
    Including a small check to test for the presence of these configuration strings to ensure your project’s portability:

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var options = {
   port: 80
 };
 
+//Including a small check to test for the presence of these configuration strings to ensure your project’s portability
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
 var bot = new TelegramBot(token, {polling: true});
 bot.getMe().then(function (me) {
   console.log('Hi my name is %s!', me.username);
